8e88c03745870e29fcbf6bc07d8f7d42a6c7ce5c
Ilkka Reponen <79361272+Shadewing89@users.noreply.github.com>
2023-06-27 17:18:54 +0300

    turn based navigation works
    next we need to check for max move distance before setting target

--- AIFinalGridEnemies/Assets/Scripts/UnitData.cs
+++ AIFinalGridEnemies/Assets/Scripts/UnitData.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 //This will use a few public ints and bools to set movement range, attack range, and turn team
@@ -8,16 +9,19 @@ public class UnitData : MonoBehaviour
 {
     public int attackRange;
     public bool onlyFurthestAttackRange;
-    public int movementRange;
+    public float movementRange;
     public int teamIndexNumber;
     private int turn;
     private bool aiTurnUsed;
     [SerializeField] List<GameObject> playerUnitsList;
     [SerializeField] List<GameObject> agentsList;
     public List<Material> teamColors;
+    public List<GameObject> swordAttackPoints;
+    public List<GameObject> spearAttackPoints;
     private MeshRenderer meshRenderer;
     public GameObject attackRangeIndicator;
     public GameObject movementTarget;
+    private GameObject closestTarget;
 
     void Start()
     {
@@ -79,13 +83,15 @@ public class UnitData : MonoBehaviour
                 break;
         }
     }
-    private GameObject FindClosestTarget(List<GameObject> agentsList)
+    private GameObject FindClosestTarget(List<GameObject> pointList)
     {
         //https://forum.unity.com/threads/clean-est-way-to-find-nearest-object-of-many-c.44315/
         GameObject tMin = null;
+        GameObject cTarget = null;
         float minDist = Mathf.Infinity;
+        float minDist2 = Mathf.Infinity;
         Vector3 currentPos = transform.position;
-        foreach (GameObject t in agentsList)
+        foreach (GameObject t in pointList)
         {
             float dist = Vector3.Distance(t.transform.position, currentPos);
             if (dist < minDist)
@@ -94,15 +100,42 @@ public class UnitData : MonoBehaviour
                 minDist = dist;
             }
         }
-        return tMin;
+        switch (attackRange) //we search attackrange positions from the target based on the weapon
+        {
+            case 1:
+                foreach (GameObject c in tMin.GetComponent<UnitData>().swordAttackPoints)
+                {
+                    float dist2 = Vector3.Distance(c.transform.position, currentPos);
+                    if (dist2 < minDist2)
+                    {
+                        cTarget = c;
+                        minDist2 = dist2;
+                    }
+                }
+                break;
+            case 2: //in the future, add if for more weapons and freeranges
+                foreach (GameObject c in tMin.GetComponent<UnitData>().spearAttackPoints)
+                {
+                    float dist2 = Vector3.Distance(c.transform.position, currentPos);
+                    if (dist2 < minDist2)
+                    {
+                        cTarget = c;
+                        minDist2 = dist2;
+                    }
+                }
+                break;
+        }
+        return cTarget;
     }
     private void AIBehaviour()
     {
         if (teamIndexNumber == 1 && aiTurnUsed == false) //only agents set to ai-team and only once
         {
+            movementTarget.SetActive(true);
             aiTurnUsed = true;
-            GameObject closestTarget = FindClosestTarget(agentsList);
-
+            closestTarget = FindClosestTarget(playerUnitsList);
+            //https://forum.unity.com/threads/find-a-point-on-a-line-between-two-vector3.140700/ next
+            movementTarget.transform.position = closestTarget.transform.position;
         }
     }
     private void OnEnable()
@@ -118,6 +151,10 @@ public class UnitData : MonoBehaviour
         if (turn == 0) //at the end of the player turn, activate ai bool with setting to false
         {
             aiTurnUsed = false;
+        }
+        if (turn == 1 && teamIndexNumber == 1)//end of ai turn
+        {
+            movementTarget.SetActive(false);
         }
         turn++;
     }
